48a5e1fb99dbaa4c44c200128f961b7046c87793
Vlad Kotliarenko <kvo@vi.ai>
2021-05-31 14:52:51 +0300

    admin cabinet updated

---
+++ assets/js/components/line-edit.js
@@ -0,0 +1,35 @@
+mTestApp.directive('lineEdit', function ($timeout) {
+    return {
+        scope: {
+            model: '=lineEdit',
+            handleSave: '&onSave',
+            handleCancel: '&onCancel'
+        },
+        link: function (scope, elm, attr) {
+            var previousValue;
+            scope.date = function () {
+                if (scope.model == "") {
+                    scope.model = "інф. відсутня"
+                }
+            }
+            scope.date()
+            scope.edit = function () {
+                scope.editMode = true;
+                previousValue = scope.model;
+                $timeout(function () {
+                    elm.find('input')[0].focus();
+                }, 0, false);
+            };
+            scope.save = function () {
+                scope.editMode = false;
+                scope.handleSave({value: scope.model});
+            };
+            scope.cancel = function () {
+                scope.editMode = false;
+                scope.model = previousValue;
+                scope.handleCancel({value: scope.model});
+            };
+        },
+        templateUrl: '/static/html/inline/line-edit.html'
+    };
+});
